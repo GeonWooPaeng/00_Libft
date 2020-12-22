5c2bb3c37ecea499770561c3ffeb2465e376ff49
GeonWooPaeng <gwpaeng@naver.com>
2020-12-22 13:45:46 +0900

    libft part1 end

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,12 +6,41 @@
 /*   By: gpaeng <gpaeng@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/21 22:49:33 by gpaeng            #+#    #+#             */
-/*   Updated: 2020/12/21 22:52:54 by gpaeng           ###   ########.fr       */
+/*   Updated: 2020/12/22 13:45:34 by gpaeng           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    *memmove(void *dest, const void *src, size_t n)
+void    *ft_memmove(void *dest, const void *src, size_t n)
 {
+    unsigned char *dtmp;
+    unsigned char *stmp;
+
+    dtmp = dest;
+    stmp = (unsigned char *)src;
+    if (!dest && !src)
+        return (0);
+    if (dest <= src)
+    {
+        while (n--)
+            *dtmp++ = *stmp++;
+    }
+    else
+    {
+        dtmp += n;
+        stmp += n;
+        while (n--)
+            *--dtmp = *--stmp;
+    }
+    return (dest);
 }
+
+// int main(void)
+// {
+//     char arr[10] = "test";
+//     char b[10] = "hipaeng";
+//     ft_memmove(arr+2, b, ft_strlen("test"));
+//     printf("%s", arr);
+//     return (0);
+// }
