323a7e2e9b8f214663e5c4688d04d5f774b8ae77
GeonWooPaeng <gwpaeng@naver.com>
2020-12-22 10:56:36 +0900

    libft part1

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: gpaeng <gpaeng@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/22 10:40:40 by gpaeng            #+#    #+#             */
-/*   Updated: 2020/12/22 10:45:24 by gpaeng           ###   ########.fr       */
+/*   Updated: 2020/12/22 10:46:37 by gpaeng           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,3 +32,24 @@ size_t  ft_strlcat(char *dest, const char *src, size_t size)
     }
     return (ldest + lsrc);
 }
+
+// int				main(void)
+// {
+// 	char	*str_base;
+// 	char	dest[100];
+// 	char	dest2[100];
+// 	char	*src;
+// 	int		index;
+
+// 	str_base = "Hello";
+// 	src = " World";
+// 	index = 0;
+// 	while (index < 6)
+// 	{
+// 		dest[index] = str_base[index];
+// 		dest2[index] = str_base[index];
+// 		index++;
+// 	}
+// 	printf("c  : (%lu) $%s$\n", strlcat(dest, src, 0), dest);
+// 	printf("ft : (%d) $%s$\n", ft_strlcat(dest2, src, 0), dest2);
+// }
